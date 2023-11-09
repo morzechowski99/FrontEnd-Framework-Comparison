3deb766268a2d3543fa367ef306349375ebc1768
Marcin Orzechowski <marcinorz@o2.pl>
2023-11-09 20:39:27 +0100

    list

---
+++ blazor/BlazorFrontEnd/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorFrontEnd.Models;
+
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? Surname { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string? Gender { get; set; }
+}
